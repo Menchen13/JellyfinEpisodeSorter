ceec2005df3163e2f281330ba387a0777a9a4646
Nik Hildebrandt <nik.hildebrandt@proton.me>
2026-04-22 22:06:52 +0200

    Outline for IdToTitle with OCR in main comments

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ int main() {
   std::map<unsigned int, std::map<unsigned int, std::string>> episodeMap;
 
   // needs to be sanity checked before actual use
+  // perhaps make url and apiKey into a FACADE struct
   std::string url = "https://jellyfin.root.adrean.eu";
   std::string apiKey = "c5d2356a93834aa5bba1de582eb1ecc0";
   std::string title = "Adventure Time";
@@ -36,7 +37,8 @@ int main() {
 
     const std::string rawSeries = fetchSeriesRaw(url, title, apiKey);
 
-    const std::vector<Series> seriesVec = parseJellyfinResponse<Series>(rawSeries);
+    const std::vector<Series> seriesVec =
+        parseJellyfinResponse<Series>(rawSeries);
 
     switch (seriesVec.size()) {
     case 0:
@@ -53,14 +55,51 @@ int main() {
 
     const std::string rawEpisodes = fetchEpisodesRaw(url, series.id, apiKey);
 
-    const std::vector<Episode> episodeVec = parseJellyfinResponse<Episode>(rawEpisodes);
+    const std::vector<Episode> episodeVec =
+        parseJellyfinResponse<Episode>(rawEpisodes);
 
-    for (const auto &e :episodeVec) {
-      std::cout << e << "\n";
-    }
-    std::cout << std::endl;
+    // asynchronously get the titleframe for each episode and ocr it.
+    // this one is gonna be tuff both have heavy IO delays
+    //
+    // Make Use of Cloud-API for ease and performance. Free tier has huge
+    // bottleneck tho create a single function which will be run on multiple
+    // threads at once and each completes a whole OCR-Cycle Use async and
+    // futures to manage easily
+    // main manages task-count and delay based on if paid-tier api-key is specified
+    // make use of expected values in threads to avoid exceptions crashing threads, or ending main function on single bad call
+    //
+    // in main:
+    // std::expected<OcrResult, std::string> IdToTitle(std::string_view url, std::string_view apiKey, std::string_view id, std::string_view googleApiKey);
+    // returns the Titlecard text for the episode passed by id. This is the threadfunction to be called in main
+    // 
+    // uses in order:
+    // opencv return-type getTitlecard(std::string_view streamUrl, unsigned int targetSecond);
+    // uses opencv to connect to the video url and only get the single frame for the titlecard. Am i correct in assuming this does not need cpr?
+    //
+    //
+    // [opencv b64 in mem]-type processTitlecard(opencv return type& image);
+    // takes a reference to the image does some math on it to prepare it for ocr and then encodes it in b64 for Google-OCR-API.
+    // Two steps bunched together since they have no dependency to wait on anything.
+    // Unit-Testable!
+    //
+    // std::string GoogleOCR(light parameter to b64 opencv obj. maybe string_view if possible, std::string_view googleApiKey);
+    // calls google OCR-API and returns the string found in the image.
+    // depending on the information the API can return (confidence values and what not) might make sense to include em in return and thread func return,
+    // for processing in main.
+    //
+    // Finally return OcrResult is just string:jellyfinId and string::titlecardText
+    //
+    // if no paid-tier is provided main will create 15 tasks and start a 1minute timer and only start the next batch when timer is up to comply with
+    // free tier.
+    // If paid-tier is provided no timer is needed once a task is done a new one is started in its place. Might also be possible to up task number decently
+    // as long as jellyfin can handle it - google should withstand virtually everthing
 
+    // seperate step in main which can also do some error handeling - if the match fails or smt interact with user.
+    // fuzzymatch the struct returned by OCR to list from Episode provider on name and on Episode structs by id.
+    // Then there is id(EpisodeStruct)->name(OcrResult)->Season&Episode number(map from provider)
+    // which can be used for last step
 
+    // fire of the apiCalls to set episode titles with id to name matches
 
   } catch (const json::exception &e) {
     std::print(stderr, "Json exception thrown: {}\n", e.what());
@@ -76,24 +115,15 @@ int main() {
   // helperPrintMaps(episodeMap);
   // std::cout << series << std::endl;
 
-  // asynchronously get the titleframe for each episode and ocr it.
-  // this one is gonna be tuff both have heavy IO delays
-  //
-  // Make Use of Cloud-API for ease and performance. Free tier has huge bottleneck tho
-  // create a single function which will be run on multiple threads at once and each completes a whole OCR-Cycle
-  // Use async and futures to manage easily
-
-  // fire of the apiCalls to set episode titles with id to name matches
-
   return 0;
 }
 
+// can throw Json::exception type
 Series promptUser(const std::vector<Series> &s) {
 
   while (true) {
     std::println("Found multiple series matching searchterm.");
-    for (const auto &[number, item] :
-         std::views::zip(std::views::iota(1), s)) {
+    for (const auto &[number, item] : std::views::zip(std::views::iota(1), s)) {
       std::println("{}: {}", number, item.name);
     }
 
@@ -106,7 +136,7 @@ Series promptUser(const std::vector<Series> &s) {
         std::from_chars(input.data(), input.data() + input.size(), selection);
 
     if (ec == std::errc() && selection > 0 && selection <= s.size()) {
-      println("Selected {}: {}", selection, s[selection - 1].name);
+      println("Selected {}: {}", selection, s.at(selection - 1).name);
       return s.at(selection - 1);
     } else {
       std::println("Incorrect selection!");
